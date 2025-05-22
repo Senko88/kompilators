c7f82671664967d3a190692c085e2de31aba465b
Senko88 <russkihkirill210@gmail.com>
2025-05-23 03:35:02 +0500

    TokenType.cs

--- TokenType.cs
+++ TokenType.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +15,7 @@ namespace kompilator
         OPERATOR,   // Оператор (+, -, :=)
         COLON,      // Двоеточие (:)
         SEMICOLON,  // Точка с запятой (;)
-        UNKNOWN     // Неизвестный символ
+        UNKNOWN ,    // Неизвестный символ
+        NEWLINE
     }
 }
